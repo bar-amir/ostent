0a6c59a7fefbc9b910c45673374cdec2912def89
Bar Amir <bar@agamonhealth.com>
2022-08-05 18:38:22 +0300

    Update folder structure

---
+++ src/components/Marker/Marker.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface MarkerProps {
+  label: string;
+}
+
+const Marker = (props: MarkerProps) => {
+  return <div>This is a marker</div>;
+};
+
+export default Marker;
